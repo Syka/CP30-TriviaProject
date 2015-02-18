c4aedda08af0eea457afbb7bd011d09d584dca1f
Deejmodin <dmodin1@confederationc.on.ca>
2015-02-18 15:21:24 -0500

    Finished trivia class, deleted unecessary code

--- app/src/main/java/com/example/jed/triviaproject/StartMenu.java
+++ app/src/main/java/com/example/jed/triviaproject/StartMenu.java
@@ -25,28 +25,6 @@ public class StartMenu extends ActionBarActivity {
     }
     public void HighScoreButtonOnClickListener(View v) {
         Intent i = new Intent(this, HighScoreScreen.class);
+        this.startActivity(i);
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
