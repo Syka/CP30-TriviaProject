f540f728a1dcecdfdf05ad8d135d54f6f17ef5bc
Deejmodin <dmodin1@confederationc.on.ca>
2015-02-19 15:59:33 -0500

    Got random questions with random arrangement of answers finished. Also clicking buttons will determine if the answer was correct.

---
+++ app/src/main/java/com/example/jed/triviaproject/Trivia.java
@@ -0,0 +1,38 @@
+package com.example.jed.triviaproject;
+
+/**
+ * Created by D.J on 2015-02-18.
+ */
+public class Trivia {
+    private String category, property, question, _a1, _a2, _a3, _ca4;
+    private String[] answers;
+
+    public Trivia(String c, String p,
+                  String q, String a1, String a2, String a3, String ca4) {
+        category = c;
+        property = p;
+        question = q;
+        _a1 = a1;
+        _a2 = a2;
+        _a3 = a3;
+        _ca4 = ca4;
+
+        answers = new String[4];
+        answers[0] = _a1;
+        answers[1] = _a2;
+        answers[2] = _a3;
+        answers[3] = _ca4;
+    }
+
+    public String getProperty() {
+        return category + "\n" + property;
+    }
+    public String getQuestion() {
+        return question;
+    }
+    public String getA_1() { return _a1; }
+    public String getA_2() { return _a2; }
+    public String getA_3() { return _a3; }
+    public String getCA_4(){ return _ca4; }
+    public String getAnswers(int index) { return answers[index]; }
+}
