cec7d1bc3d2e13f3012bed4095befc212482fc63
Jordan <jwolkows@confederationc.on.ca>
2015-02-23 19:27:47 -0500

    Updated Questions

--- app/src/main/java/com/example/jed/triviaproject/TriviaDriver.java
+++ app/src/main/java/com/example/jed/triviaproject/TriviaDriver.java
@@ -14,12 +14,50 @@ public class TriviaDriver {
         //      WRONG ANSWER 1, WRONG ANSWER 2, WRONG ANSWER 3, CORRECT ANSWER);
 
         //MOVIE TRIVIA
+
+        //Star Wars
         trivia.add(new Trivia("Movies", "Star Wars",
                 "What is the probability of successfully navigating an asteroid field?",
                 "1,547:1", "1,245,677:1", "10:1", "3,720:1"));
+        trivia.add(new Trivia("Movies", "Star Wars",
+                "The volcanic world of Mustafar in Episode 3: Revenge of the Sith was designed to look like what?",
+                "George Lucas’s Vision of the Sun.", "George Lucas’s Worst Nightmare", "Florida", "George Lucas’s Vision of Hell."));
+
+        //Marvel
         trivia.add(new Trivia("Movies", "Marvel",
                 "What does Rocket Raccoon require to escape the prison in Guardians of the Galaxy? ",
                 "An Infinity Stone", "A keycard", "Stan Lee", "A prosthetic leg"));
+        trivia.add(new Trivia("Movies", "Marvel",
+                "Of all the Avengers, which movie star appeared as a different superhero in 2005?",
+                "Scarlett Johansson", "Robert Downey Jr.", "Chris Hemsworth", "Chris Evans"));
+
+        //Back to the Future
+        trivia.add(new Trivia("Movies", "Back to the Future",
+                "How many jigawatts are needed to go back in time?",
+                "2.09", "1.25", "2.03", "1.21"));
+        trivia.add(new Trivia("Movies", "Back to the Future",
+                "Make like a tree and ...?",
+                "Stay Put", "Die Old", "Travel Light", "Get Lost"));
+
+        //Lord of the Rings
+        trivia.add(new Trivia("Movies", "Lord of the Rings",
+                "Other then the movies, Gollum showed up on what awards show? ",
+                "Kids Choice Awards", "Emmys", "The Oscars", "MTV Video Awards"));
+        trivia.add(new Trivia("Movies", "Lord of the Rings",
+                "Whats the most valuable thing in the Lonely Mountain?",
+                "Gollum", "The Ring", "Smaug", "Arkenstone"));
+
+        //Die Hard
+        trivia.add(new Trivia("Movies", "Die Hard",
+                "What do most cops do with their badge numbers?",
+                "Race cars", "Gamble the badge", "Stop Crimes", "Play the lottery"));
+        trivia.add(new Trivia("Movies", "Die Hard",
+                "What is the name of Mcleans partner in Die Hard 3?",
+                "Posidon", "Hades", "Athena", "Zeus"));
+
+
+
+
 
         //TV TRIVIA
         trivia.add(new Trivia("Television", "The Simpsons",
